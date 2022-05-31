dfd6d7889b86ce86148b9cd1de06c647bf715789
Jichen Liu <jichen.liu85@gmail.com>
2022-05-30 22:01:50 -0400

    vnet mysql-13

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -16,7 +16,6 @@ class Controller extends BaseController
     public function save()
     {   
 		//echo "12345";
-		echo env('DB_USERNAME');
         $result=DB::table('users')->insert(
             ["id" => "1b7161ea8542462dbf21db".mt_rand(1,1000000),
                 'name' => 'sam',
